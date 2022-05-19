d9bd97b292f9b8693050b2a4f28be1ba83162ca0
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-19 16:17:25 +0300

    Update WeatherInfoInstance.kt
    * Add CallAdapterFactory

--- app/src/main/java/com/dozmaden/weatherapp/api/WeatherInfoInstance.kt
+++ app/src/main/java/com/dozmaden/weatherapp/api/WeatherInfoInstance.kt
@@ -1,5 +1,6 @@
 package com.dozmaden.weatherapp.api
 
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,7 +16,7 @@ object WeatherInfoInstance {
         Retrofit.Builder()
             .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create())
-//            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .baseUrl(BASE_URL)
             .build()
 
