838ef122aee245a718510a7d69a98493e6abbe77
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 12:18:37 +0300

    Create WeatherInfo.kt

---
+++ app/src/main/java/com/dozmaden/weatherapp/dto/WeatherInfo.kt
@@ -0,0 +1,17 @@
+package com.dozmaden.weatherapp.dto
+
+data class WeatherInfo (
+    val coord: Coordinates,
+    val weather: List<Weather>,
+    val base: String,
+    val main: MainInfo,
+    val visibility: Int,
+    val wind: Wind,
+    val clouds: Clouds,
+    val dt: Int,
+    val sys: ApiSystemInfo,
+    val timezone: ApiTimezone,
+    val id: Int,
+    val name: String,
+    val cod: Int
+)
