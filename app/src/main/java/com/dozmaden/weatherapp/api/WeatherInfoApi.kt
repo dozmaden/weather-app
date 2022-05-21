717092010c678583cd92a63d3257774c526ad23a
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 11:49:48 +0300

    Specify API
    * Will use the OpenWeather API with longitude & latitude calls

--- app/src/main/java/com/dozmaden/weatherapp/api/WeatherInfoApi.kt
+++ app/src/main/java/com/dozmaden/weatherapp/api/WeatherInfoApi.kt
@@ -1,10 +1,15 @@
 package com.dozmaden.weatherapp.api
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
-const val BASE_URL = "https://api.openweathermap.org/"
+const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
 interface WeatherInfoApi {
-    @GET(BASE_URL)
-    fun getWeatherInfo()
+    @GET("/weather?lat={lat}&lon={lon}&appid={api}")
+    fun getWeatherInfo(
+        @Path("lat") latitude: String,
+        @Path("lon") longitude: String,
+        @Path("api") apiKey: String
+    )
 }
