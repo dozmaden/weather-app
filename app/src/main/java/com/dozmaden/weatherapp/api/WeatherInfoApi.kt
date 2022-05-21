52e81f3944ad9ffb9ccf32039284394561e6a542
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 12:14:33 +0300

    Add Units parameter to WeatherInfo
    Add Units

--- app/src/main/java/com/dozmaden/weatherapp/api/WeatherInfoApi.kt
+++ app/src/main/java/com/dozmaden/weatherapp/api/WeatherInfoApi.kt
@@ -6,10 +6,11 @@ import retrofit2.http.Path
 const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
 interface WeatherInfoApi {
-    @GET("/weather?lat={lat}&lon={lon}&appid={api}")
+    @GET("/weather?lat={lat}&lon={lon}&units={units}&appid={api}")
     fun getWeatherInfo(
         @Path("lat") latitude: String,
         @Path("lon") longitude: String,
+        @Path("units") units: String,
         @Path("api") apiKey: String
     )
 }
