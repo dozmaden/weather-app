166e770a47bb6cd437bddda44c883be9f8b51541
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 15:07:08 +0300

    Update GeocodingApi.kt
    * Geocoding APIs JSON response returns a list

--- app/src/main/java/com/dozmaden/weatherapp/api/GeocodingApi.kt
+++ app/src/main/java/com/dozmaden/weatherapp/api/GeocodingApi.kt
@@ -1,5 +1,6 @@
 package com.dozmaden.weatherapp.api
 
+import com.dozmaden.weatherapp.dto.Location
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -11,12 +12,12 @@ interface GeocodingApi {
     fun directGeocoding(
         @Path("city") city: String,
         @Path("api") apiKey: String
-    )
+    ) : List<Location>
 
     @GET("/reverse?lat={lat}&lon={lon}&limit=1&appid={api}")
     fun reverseGeocoding(
         @Path("lat") latitude: Double,
         @Path("lon") longitude: Double,
         @Path("api") apiKey: String
-    )
+    ) : List<Location>
 }
