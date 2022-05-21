020fabdead6e4f54f833fa2fa006b77b8f4a92a2
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 13:40:39 +0300

    Adopt OneCallApi
    * Changed WeatherInfo to match OneCallApi response
    * Added additional DTOs

---
+++ app/src/main/java/com/dozmaden/weatherapp/dto/FeelsLike.kt
@@ -0,0 +1,8 @@
+package com.dozmaden.weatherapp.dto
+
+data class FeelsLike(
+    val day: Double,
+    val night: Double,
+    val eve: Double,
+    val morn: Double
+)
--- app/src/main/java/com/dozmaden/weatherapp/dto/WeatherInfo.kt
+++ app/src/main/java/com/dozmaden/weatherapp/dto/WeatherInfo.kt
@@ -1,17 +1,12 @@
 package com.dozmaden.weatherapp.dto
 
 data class WeatherInfo (
-    val coord: Coordinates,
-    val weather: List<Weather>,
-    val base: String,
-    val main: MainInfo,
-    val visibility: Int,
-    val wind: Wind,
-    val clouds: Clouds,
-    val dt: Int,
-    val sys: ApiSystemInfo,
-    val timezone: Int,
-    val id: Int,
-    val name: String,
-    val cod: Int
+    val lon: Double,
+    val lat: Double,
+    val timezone: String,
+    val timezone_offset: Int,
+    val current: CurrentWeather,
+    val hourly: List<HourlyWeather>,
+    val daily: List<DailyWeather>,
+//    val alerts: List<Alert>
 )
