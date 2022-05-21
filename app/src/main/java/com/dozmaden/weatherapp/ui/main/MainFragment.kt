dfd1395df5e68d7dcde43e94cef76ab5dca6a300
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 17:58:27 +0300

    Update MainFragment.kt

--- app/src/main/java/com/dozmaden/weatherapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/dozmaden/weatherapp/ui/main/MainFragment.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.dozmaden.weatherapp.R
+import com.dozmaden.weatherapp.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
+    private var _binding: MainFragmentBinding? = null
+
+        // This property is only valid between onCreateView and
+        // onDestroyView.
+                                   private val binding get() = _binding!!
 
     private lateinit var viewModel: MainViewModel
 
@@ -20,13 +22,11 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 }
