70f4231c36980816eb8a17bc9f8afbd4d7981394
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 12:30:41 +0300

    Update MainInfo.kt

--- app/src/main/java/com/dozmaden/weatherapp/dto/MainInfo.kt
+++ app/src/main/java/com/dozmaden/weatherapp/dto/MainInfo.kt
@@ -6,5 +6,7 @@ data class MainInfo(
     val temp_min: Double,
     val temp_max: Double,
     val pressure: Int,
-    val humidity: Int
+    val humidity: Int,
+    val sea_level: Int,
+    val grnd_level: Int
 )
