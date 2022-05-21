fec4044ff33f5b019aa1990665d2f5392b49264a
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 15:10:04 +0300

    Create GeocodingInstance.kt

---
+++ app/src/main/java/com/dozmaden/weatherapp/api/GeocodingInstance.kt
@@ -0,0 +1,24 @@
+package com.dozmaden.weatherapp.api
+
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object GeocodingInstance {
+    private val logInterceptor =
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+    private val httpClient = OkHttpClient.Builder().addNetworkInterceptor(logInterceptor).build()
+
+    private val retrofit =
+        Retrofit.Builder()
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .baseUrl(GEOCODING_URL)
+            .build()
+
+    val GEOCODING_API: GeocodingApi = retrofit.create(GeocodingApi::class.java)
+}
