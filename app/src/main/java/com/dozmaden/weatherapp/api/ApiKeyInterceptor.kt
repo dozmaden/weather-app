a1854d69872c5d3bde09fcdaad0d3208ed8a8609
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 18:44:01 +0300

    Create ApiKeyInterceptor.kt

---
+++ app/src/main/java/com/dozmaden/weatherapp/api/ApiKeyInterceptor.kt
@@ -0,0 +1,22 @@
+package com.dozmaden.weatherapp.api
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+private const val ApiKeyOpenWeather = "e9e490db954584052230aaaf3f695002"
+
+class ApiKeyInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val original: Request = chain.request()
+        val originalUrl: HttpUrl = original.url
+        val url = originalUrl.newBuilder()
+            .addQueryParameter("appid", ApiKeyOpenWeather)
+            .build()
+        val requestBuilder: Request.Builder = original.newBuilder().url(url)
+        val request: Request = requestBuilder.build()
+        return chain.proceed(request)
+    }
+}
