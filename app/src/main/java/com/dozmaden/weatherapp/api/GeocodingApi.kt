216d1011899353cd3ae67d6f1695f2cfefce3a7b
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 14:18:55 +0300

    Create GeocodingApi.kt

---
+++ app/src/main/java/com/dozmaden/weatherapp/api/GeocodingApi.kt
@@ -0,0 +1,22 @@
+package com.dozmaden.weatherapp.api
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+const val GEOCODING_URL = "http://api.openweathermap.org/geo/1.0/"
+
+interface GeocodingApi {
+
+    @GET("/direct?q={city}&limit=5appid={api}")
+    fun directGeocoding(
+        @Path("city") city: String,
+        @Path("api") apiKey: String
+    )
+
+    @GET("/reverse?lat={lat}&lon={lon}&limit=1&appid={api}")
+    fun reverseGeocoding(
+        @Path("lat") latitude: Double,
+        @Path("lon") longitude: Double,
+        @Path("api") apiKey: String
+    )
+}
