095c6499d986f12858181595eb7256abce23f894
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-21 13:05:46 +0300

    Begin migration to OneCallApi

--- app/src/main/java/com/dozmaden/weatherapp/api/WeatherInfoApi.kt
+++ app/src/main/java/com/dozmaden/weatherapp/api/WeatherInfoApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Path
 const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
 interface WeatherInfoApi {
-    @GET("/weather?lat={lat}&lon={lon}&units={units}&appid={api}")
+    @GET("/onecall?lat={lat}&lon={lon}&units={units}&exclude=minutely&appid={api}")
     fun getWeatherInfo(
         @Path("lat") latitude: String,
         @Path("lon") longitude: String,
