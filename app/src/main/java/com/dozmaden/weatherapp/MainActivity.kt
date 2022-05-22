a0d23da180ddebe28ebc4742325d8eeab56a92d5
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-22 19:23:14 +0300

    Update MainActivity.kt

--- app/src/main/java/com/dozmaden/weatherapp/MainActivity.kt
+++ app/src/main/java/com/dozmaden/weatherapp/MainActivity.kt
@@ -2,17 +2,11 @@ package com.dozmaden.weatherapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dozmaden.weatherapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
