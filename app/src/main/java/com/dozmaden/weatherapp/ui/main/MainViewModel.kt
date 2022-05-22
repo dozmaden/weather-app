200318fcc0a796b170c30d7aa7ed93788097b2bc
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-22 16:18:09 +0300

    Add geolocation to MainViewModel

--- app/src/main/java/com/dozmaden/weatherapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dozmaden/weatherapp/ui/main/MainViewModel.kt
@@ -1,7 +1,25 @@
 package com.dozmaden.weatherapp.ui.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.location.Location
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.dozmaden.weatherapp.GeolocationProvider
+import com.dozmaden.weatherapp.dto.WeatherInfo
+import com.dozmaden.weatherapp.utils.GeolocationUtility
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val geolocationProvider: GeolocationProvider =
+        GeolocationUtility.getGeolocationProvider(application)
+
+    private val _currentGeolocation = MutableLiveData<Location>()
+    internal val currentGeolocation: LiveData<Location> = _currentGeolocation.apply {
+        postValue(geolocationProvider.getLocation())
+    }
+
     // TODO: Implement the ViewModel
 }
