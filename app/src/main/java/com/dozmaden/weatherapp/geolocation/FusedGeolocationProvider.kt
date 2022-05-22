735feccc0d7eff291ed2b6298c1228bdbbb67d47
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-22 20:39:44 +0300

    Update FusedGeolocationProvider.kt

--- app/src/main/java/com/dozmaden/weatherapp/geolocation/FusedGeolocationProvider.kt
+++ app/src/main/java/com/dozmaden/weatherapp/geolocation/FusedGeolocationProvider.kt
@@ -1,23 +1,63 @@
 package com.dozmaden.weatherapp.geolocation
 
-import android.annotation.SuppressLint
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationRequest.PRIORITY_LOW_POWER
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 
-internal class FusedGeolocationProvider(context: Context) : AbstractGeolocationProvider() {
+internal class FusedGeolocationProvider(private val context: Context) :
+    AbstractGeolocationProvider() {
 
     private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
-    @SuppressLint("MissingPermission")
-    override fun getLocation(): Location {
-        fusedLocationClient.lastLocation.addOnSuccessListener { fusedLocation: Location? ->
-            if (fusedLocation != null) run { currentLocation = fusedLocation }
-            else {
-                Log.d("FusedLocationProvider", "NULL LOCATION!")
-            }
+    override fun getLocation(): Location? {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.i("IMHERE", "nopermissions")
+        } else {
+            Log.i("IMHERE", "havePermissions")
+
+            fusedLocationClient
+                .getCurrentLocation(
+                    PRIORITY_LOW_POWER,
+                    object : CancellationToken() {
+                        override fun onCanceledRequested(p0: OnTokenCanceledListener) =
+                            CancellationTokenSource().token
+                        override fun isCancellationRequested() = false
+                    }
+                )
+                .addOnSuccessListener {
+                    currentLocation = it
+                    Log.i("IMHERE", "CURRENT LOCATION SUCCESS")
+                    Log.i("IMHERE", it.latitude.toString())
+                    Log.i("IMHERE", it.longitude.toString())
+                }
         }
-        return super.getLocation()
+        if (currentLocation == null) {
+            Log.i("IMHERE", "currentLocation = null")
+        }
+
+        Log.i("IMHERE", "currentLocation")
+
+        return currentLocation
     }
 }
