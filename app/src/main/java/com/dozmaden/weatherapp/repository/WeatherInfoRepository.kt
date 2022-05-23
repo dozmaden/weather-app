e29d6a3ae714f04af18c8039d46eee3c9335a578
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-23 16:16:12 +0300

    Refactor Repositories

--- app/src/main/java/com/dozmaden/weatherapp/repository/WeatherInfoRepository.kt
+++ app/src/main/java/com/dozmaden/weatherapp/repository/WeatherInfoRepository.kt
@@ -1,13 +1,13 @@
 package com.dozmaden.weatherapp.repository
 
 import com.dozmaden.weatherapp.api.WeatherInfoInstance
-import com.dozmaden.weatherapp.dto.WeatherInfo
+import com.dozmaden.weatherapp.dto.Weathers
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 object WeatherInfoRepository {
-    internal fun getWeatherInfo(lat: Double, lon: Double, units: String): Single<WeatherInfo> =
+    internal fun getWeatherInfo(lat: Double, lon: Double, units: String): Single<Weathers> =
         WeatherInfoInstance.WEATHER_API.getWeatherInfo(lat, lon, units)
             .retry(4L)
             .delay(300L, TimeUnit.MILLISECONDS, true)
