e11ab9c4829353567f50471d69fde839f296c4a0
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-23 22:10:39 +0300

    Refactor WeatherInfo to WeatherData

--- app/src/main/java/com/dozmaden/weatherapp/repository/WeatherDataRepository.kt
+++ app/src/main/java/com/dozmaden/weatherapp/repository/WeatherDataRepository.kt
@@ -1,15 +1,18 @@
 package com.dozmaden.weatherapp.repository
 
+import android.util.Log
 import com.dozmaden.weatherapp.api.WeatherInfoInstance
 import com.dozmaden.weatherapp.dto.Weathers
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
-object WeatherInfoRepository {
-    internal fun getWeatherInfo(lat: Double, lon: Double, units: String): Single<Weathers> =
-        WeatherInfoInstance.WEATHER_API.getWeatherInfo(lat, lon, units)
+object WeatherDataRepository {
+    internal fun getWeatherData(lat: Double, lon: Double, units: String): Single<Weathers> {
+        Log.i("WeatherDataRepository", "Getting weather from API!")
+        return WeatherInfoInstance.WEATHER_API.getWeatherInfo(lat, lon, units)
             .retry(4L)
             .delay(300L, TimeUnit.MILLISECONDS, true)
             .subscribeOn(Schedulers.io())
+    }
 }
