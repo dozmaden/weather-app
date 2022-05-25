8ff1456492c110033d3d2380b400221f4540cd41
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-25 15:13:05 +0300

    Location & cache logic
    * Replaced FusedLocation with LocationManager
    * Check cache before repository query
    * Try reverse geocode

--- app/src/main/java/com/dozmaden/weatherapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dozmaden/weatherapp/ui/main/MainViewModel.kt
@@ -1,28 +1,28 @@
 package com.dozmaden.weatherapp.ui.main
 
+import android.Manifest
 import android.app.Application
-import android.location.Location
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.dozmaden.weatherapp.dto.CurrentWeather
 import com.dozmaden.weatherapp.dto.DayWeather
 import com.dozmaden.weatherapp.dto.HourWeather
-import com.dozmaden.weatherapp.geolocation.GeolocationProvider
-import com.dozmaden.weatherapp.geolocation.GeolocationProviderFactory
 import com.dozmaden.weatherapp.preferences.WeatherPreferences
+import com.dozmaden.weatherapp.repository.GeocodingRepository
 import com.dozmaden.weatherapp.repository.WeatherDataRepository
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.kotlin.subscribeBy
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val geolocationProvider: GeolocationProvider =
-        GeolocationProviderFactory.getGeolocationProvider(application)
-
-    private val _currentGeolocation = MutableLiveData<Location?>()
-    internal val currentGeolocation: LiveData<Location?> = _currentGeolocation
+    private val _currentGeolocationName = MutableLiveData<String>()
+    internal val currentGeolocationName: LiveData<String> = _currentGeolocationName
 
     private val _currentWeatherInfo = MutableLiveData<CurrentWeather>()
     internal val currentWeatherInfo: LiveData<CurrentWeather> = _currentWeatherInfo
@@ -36,26 +36,67 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val weatherCache = WeatherPreferences(application)
 
     internal fun getWeatherData() {
-        val lat = 55.7558
-        val lon = 37.6173
 
-        Log.i("MainViewModel", "Getting data from Repository!")
-        WeatherDataRepository.getWeatherData(lat, lon, "metric")
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeBy(
-                onSuccess = { weathers ->
-                    Log.i("MainViewModel", "Got weather response from Repository!")
-                    _currentWeatherInfo.postValue(weathers.current)
-                    _dailyWeatherInfo.postValue(weathers.daily)
-                    _hourlyWeatherInfo.postValue(weathers.hourly)
-                    weatherCache.saveWeatherData(weathers)
-                },
-                onError = {
-                    Log.i("MainViewModel", "Didn't get weather from Repository!")
-                    weatherCache.getCurrentWeatherCache()?.let { _currentWeatherInfo.postValue(it) }
-                    weatherCache.getDailyWeatherCache()?.let { _dailyWeatherInfo.postValue(it) }
-                    weatherCache.getHourlyWeatherCache()?.let { _hourlyWeatherInfo.postValue(it) }
-                }
-            )
+        val locprovider =
+            getApplication<Application>().getSystemService(Context.LOCATION_SERVICE)
+                as LocationManager
+
+        if (ActivityCompat.checkSelfPermission(
+                getApplication(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    getApplication(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+        val location = locprovider.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER)
+        //        val location = geolocationProvider.getLocation()
+
+        location?.let {
+            Log.i("MainViewModel", "Getting data from repsository!")
+            WeatherDataRepository.getWeatherData(location.latitude, location.longitude, "metric")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onSuccess = { weathers ->
+                        Log.i("MainViewModel", "Got weather response from Repository!")
+                        _currentWeatherInfo.postValue(weathers.current)
+                        _dailyWeatherInfo.postValue(weathers.daily)
+                        _hourlyWeatherInfo.postValue(weathers.hourly)
+                        weatherCache.saveWeatherData(weathers)
+                    },
+                    onError = { Log.i("MainViewModel", "Didn't get weather from Repository!") }
+                )
+
+            GeocodingRepository.reverseGeocode(location.latitude, location.longitude)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onSuccess = {
+                        Log.i("MainViewModel", "Got reverse geocode response!")
+                        _currentGeolocationName.postValue(it[0].name)
+                    },
+                    onError = {
+                        Log.i("MainViewModel", "Failed to get reverse geocode response :(")
+                    }
+                )
+        }
+    }
+
+    internal fun loadWeatherCache() {
+        weatherCache.getCurrentWeatherCache()?.let { current ->
+            _currentWeatherInfo.postValue(current)
+        }
+        weatherCache.getDailyWeatherCache()?.let { daily -> _dailyWeatherInfo.postValue(daily) }
+        weatherCache.getHourlyWeatherCache()?.let { hourly -> _hourlyWeatherInfo.postValue(hourly) }
     }
 }
