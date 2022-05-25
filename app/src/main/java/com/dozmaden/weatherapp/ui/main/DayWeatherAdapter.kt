b84fb1116ab5148cd0a2e5706b018960d442d528
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-25 19:25:40 +0300

    Refactor Adapters

--- app/src/main/java/com/dozmaden/weatherapp/ui/main/DayWeatherAdapter.kt
+++ app/src/main/java/com/dozmaden/weatherapp/ui/main/DayWeatherAdapter.kt
@@ -1,68 +1,52 @@
 package com.dozmaden.weatherapp.ui.main
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.dozmaden.weatherapp.R
-import com.dozmaden.weatherapp.dto.DayWeather
+import com.dozmaden.weatherapp.dto.DailyWeather
+import com.dozmaden.weatherapp.utils.WeatherIconLoadingUtility
 import java.text.SimpleDateFormat
 
-class DayWeatherAdapter(private val mList: List<DayWeather>) :
+class DayWeatherAdapter(private val mList: List<DailyWeather>) :
     RecyclerView.Adapter<DayWeatherAdapter.ViewHolder>() {
 
-    // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // inflates the card_view_design view
-        // that is used to hold list item
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_daily_weather, parent, false)
 
         return ViewHolder(view)
     }
 
-    // binds the list items to a view
+    @SuppressLint("SimpleDateFormat")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        //        val ItemsViewModel = mList[position]
-
-        // sets the image to the imageview from our itemHolder class
-        //        holder.imageView.setImageResource(ItemsViewModel.image)
-
-        // sets the text to the textview from our itemHolder class
         holder.dayTemperatureTextView.text =
             holder.dayTemperatureTextView.resources
                 .getString(R.string.celcius_temperature)
-                .format(mList[position].temp.min.toString())
+                .format(mList[position].temp.min.toString().substringBefore("."))
 
         holder.dayDescriptionTextView.text = mList[position].weather[0].main
 
-        //        val simpleDate = SimpleDateFormat("dd/MM/YYYY hh:mm:ss")
-        val simpleDate = SimpleDateFormat("EEE, MMM d")
-        val currentDate = simpleDate.format(mList[position].dt.toLong() * 1000)
+        holder.dayTimeTextView.text =
+            SimpleDateFormat("EEE, MMM d").format(mList[position].dt.toLong() * 1000)
 
-        holder.dayTimeTextView.text = currentDate
-
-        Glide.with(holder.dayWeatherImageView.context)
-            .load(
-                "https://openweathermap.org/img/wn/" + mList[position].weather[0].icon + "@2x.png"
-            )
-            .centerCrop()
-            //                    .placeholder()
-            .into(holder.dayWeatherImageView)
+        WeatherIconLoadingUtility.loadImage(
+            holder.dayWeatherImageView.context,
+            mList[position].weather[0].icon,
+            holder.dayWeatherImageView
+        )
     }
 
-    // return the number of the items in the list
     override fun getItemCount(): Int {
         return mList.size
     }
 
-    // Holds the views for adding it to image and text
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        //        val imageView: ImageView = itemView.findViewById(R.id.imageview)
         val dayTemperatureTextView: TextView = itemView.findViewById(R.id.day_temperature)
         val dayTimeTextView: TextView = itemView.findViewById(R.id.day_time)
         val dayDescriptionTextView: TextView = itemView.findViewById(R.id.day_main_description)
--- app/src/main/java/com/dozmaden/weatherapp/ui/main/HourlyWeatherAdapter.kt
+++ app/src/main/java/com/dozmaden/weatherapp/ui/main/HourlyWeatherAdapter.kt
@@ -1,44 +1,32 @@
 package com.dozmaden.weatherapp.ui.main
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.dozmaden.weatherapp.R
-import com.dozmaden.weatherapp.dto.HourWeather
+import com.dozmaden.weatherapp.dto.HourlyWeather
+import com.dozmaden.weatherapp.utils.WeatherIconLoadingUtility
 import java.text.SimpleDateFormat
 
-class HourlyWeatherAdapter(private val mList: List<HourWeather>) :
+class HourlyWeatherAdapter(private val mList: List<HourlyWeather>) :
     RecyclerView.Adapter<HourlyWeatherAdapter.ViewHolder>() {
 
-    // create new views
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // inflates the card_view_design view
-        // that is used to hold list item
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_hourly_weather, parent, false)
 
         return ViewHolder(view)
     }
 
-    // binds the list items to a view
+    @SuppressLint("SimpleDateFormat")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        //        val ItemsViewModel = mList[position]
-
-        // sets the image to the imageview from our itemHolder class
-        //        holder.imageView.setImageResource(ItemsViewModel.image)
-
-        // sets the text to the textview from our itemHolder class
-
-        //        val simpleDate = SimpleDateFormat("dd/MM/YYYY hh:mm:ss")
-        val simpleDate = SimpleDateFormat("HH aa")
-        val currentDate = simpleDate.format(mList[position].dt.toLong() * 1000)
-
-        holder.hourlyTimeTextView.text = currentDate
+        holder.hourlyTimeTextView.text =
+            SimpleDateFormat("HH aa").format(mList[position].dt.toLong() * 1000)
 
         holder.hourlyTemperatureTextView.text =
             holder.hourlyTemperatureTextView.resources
@@ -46,28 +34,23 @@ class HourlyWeatherAdapter(private val mList: List<HourWeather>) :
                 .format(mList[position].temp.toString().substringBefore("."))
 
         holder.hourlyDescriptionTextView.text = mList[position].weather[0].main
-        Glide.with(holder.hourlyWeatherImageView.context)
-            .load(
-                "https://openweathermap.org/img/wn/" + mList[position].weather[0].icon + "@2x.png"
-            )
-            .centerCrop()
-            //                    .placeholder()
-            .into(holder.hourlyWeatherImageView)
+
+        WeatherIconLoadingUtility.loadImage(
+            holder.hourlyWeatherImageView.context,
+            mList[position].weather[0].icon,
+            holder.hourlyWeatherImageView
+        )
     }
 
-    // return the number of the items in the list
     override fun getItemCount(): Int {
         return mList.size
     }
 
-    // Holds the views for adding it to image and text
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        //        val imageView: ImageView = i/temView.findViewById(R.id.imageview)
         val hourlyTimeTextView: TextView = itemView.findViewById(R.id.hourly_time)
         val hourlyTemperatureTextView: TextView = itemView.findViewById(R.id.hourly_temperature)
         val hourlyDescriptionTextView: TextView =
             itemView.findViewById(R.id.hourly_main_description)
-        val hourlyWeatherImageView: ImageView =
-            itemView.findViewById<ImageView?>(R.id.hourly_weather_image)
+        val hourlyWeatherImageView: ImageView = itemView.findViewById(R.id.hourly_weather_image)
     }
 }
