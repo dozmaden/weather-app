0162e978aba6538e2a61be2d1112cb05248bf0b0
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-25 19:17:25 +0300

    Update WeatherDataRepository.kt

--- app/src/main/java/com/dozmaden/weatherapp/repository/WeatherDataRepository.kt
+++ app/src/main/java/com/dozmaden/weatherapp/repository/WeatherDataRepository.kt
@@ -2,14 +2,14 @@ package com.dozmaden.weatherapp.repository
 
 import android.util.Log
 import com.dozmaden.weatherapp.api.OpenWeatherInstance
-import com.dozmaden.weatherapp.dto.Weathers
-import com.dozmaden.weatherapp.dto.geocoding_dto
+import com.dozmaden.weatherapp.dto.GeocodeArray
+import com.dozmaden.weatherapp.dto.WeatherData
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 object WeatherDataRepository {
-    internal fun getWeatherData(lat: Double, lon: Double, units: String): Single<Weathers> {
+    internal fun getWeatherData(lat: Double, lon: Double, units: String): Single<WeatherData> {
         Log.i("WeatherDataRepository", "Getting weather from API!")
         return OpenWeatherInstance.OPEN_WEATHER_API.getWeatherInfo(lat, lon, units)
             .retry(4L)
@@ -17,14 +17,8 @@ object WeatherDataRepository {
             .subscribeOn(Schedulers.io())
     }
 
-    //    internal fun geocoding(city: String) {
-    //        return OpenWeatherInstance.OPEN_WEATHER_API.directGeocoding(city)
-    //            .retry(4L)
-    //            .delay(300L, TimeUnit.MILLISECONDS, true)
-    //            .subscribeOn(Schedulers.io())
-    //    }
-
-    internal fun reverseGeocoding(lat: Double, lon: Double): Single<geocoding_dto> {
+    internal fun reverseGeocoding(lat: Double, lon: Double): Single<GeocodeArray> {
+        Log.i("WeatherDataRepository", "Reverse geocoding location from API!")
         return OpenWeatherInstance.OPEN_WEATHER_API.reverseGeocoding(lat, lon)
             .retry(4L)
             .delay(300L, TimeUnit.MILLISECONDS, true)
