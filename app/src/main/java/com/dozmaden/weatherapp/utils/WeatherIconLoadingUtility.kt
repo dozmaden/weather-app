0d32248a73fbe447bc2027dcba26ab6cd0217159
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-25 19:25:43 +0300

    Create WeatherIconLoadingUtility.kt

---
+++ app/src/main/java/com/dozmaden/weatherapp/utils/WeatherIconLoadingUtility.kt
@@ -0,0 +1,17 @@
+package com.dozmaden.weatherapp.utils
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+object WeatherIconLoadingUtility {
+    fun loadImage(context: Context, icon: String, image: ImageView) {
+        Glide.with(context)
+            .load(
+                "https://openweathermap.org/img/wn/$icon@2x.png"
+            )
+            .centerCrop()
+            //                    .placeholder()
+            .into(image)
+    }
+}
