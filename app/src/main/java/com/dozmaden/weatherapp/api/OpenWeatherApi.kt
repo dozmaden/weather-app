54eef1e3ad45a4e88dff9b4a20c4c7b3b0b68f21
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-05-25 19:09:06 +0300

    Update OpenWeatherApi.kt

--- app/src/main/java/com/dozmaden/weatherapp/api/OpenWeatherApi.kt
+++ app/src/main/java/com/dozmaden/weatherapp/api/OpenWeatherApi.kt
@@ -1,7 +1,7 @@
 package com.dozmaden.weatherapp.api
 
-import com.dozmaden.weatherapp.dto.Weathers
-import com.dozmaden.weatherapp.dto.geocoding_dto
+import com.dozmaden.weatherapp.dto.GeocodeArray
+import com.dozmaden.weatherapp.dto.WeatherData
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,16 +9,15 @@ import retrofit2.http.Query
 const val BASE_URL = "https://api.openweathermap.org/"
 
 interface OpenWeatherApi {
-    //    @GET("/onecall?lat={lat}&lon={lon}&units={units}&exclude=minutely,alerts")
     @GET("/data/2.5/onecall")
-    // &appid={api}
     fun getWeatherInfo(
         @Query("lat") latitude: Double,
         @Query("lon") longitude: Double,
         @Query("units") units: String,
         //        @Path("api") apiKey: String
         @Query("exclude") exclude: String = "minutely,alerts"
-    ): Single<Weathers>
+    ): Single<WeatherData>
+
     //
     //    @GET("/direct")
     //    fun directGeocoding(
@@ -31,5 +30,5 @@ interface OpenWeatherApi {
         @Query("lat") latitude: Double,
         @Query("lon") longitude: Double,
         @Query("limit") limit: Int = 1,
-    ): Single<geocoding_dto>
+    ): Single<GeocodeArray>
 }
